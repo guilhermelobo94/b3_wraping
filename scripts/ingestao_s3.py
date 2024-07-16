989606269aa31a72e13d43f600045f27dcd25581
Guilherme Lobo <guilhermegclobo94@gmail.com>
2024-07-15 22:16:07 -0300

    Ajuste no bucket

--- scripts/ingestao_s3.py
+++ scripts/ingestao_s3.py
@@ -1,29 +1,29 @@
 import boto3
-import os
 from datetime import datetime
+from botocore.exceptions import ClientError
 
 s3_client = boto3.client(
     's3',
-    aws_access_key_id='fiap_b3',
-    aws_secret_access_key='fiap_b3@',
+    aws_access_key_id='AKIAQDRB6H65UPQSE2LJ',
+    aws_secret_access_key='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',
     region_name='us-east-1'
 )
 
 today = datetime.today().strftime('%Y-%m-%d')
-
 parquet_file = f'dados_{today}.parquet'
 
-s3_client = boto3.client('s3')
-
-directory_name = 'parquet_data'
-
-bucket_name = 'bucket_fiap'
-
+directory_name = 'parquet'
+bucket_name = 'bucketfiap'
 s3_path = f'{directory_name}/dados_{today}.parquet'
 
-
-s3_client.upload_file(parquet_file, bucket_name, s3_path)
-
-os.remove(parquet_file)
-
-print(f'Arquivo {parquet_file} enviado para s3://{bucket_name}/{s3_path}')
+try:
+    s3_client.upload_file(parquet_file, bucket_name, s3_path)
+    print("Upload bem-sucedido!")
+except ClientError as e:
+    error_code = e.response['Error']['Code']
+    if error_code == 'SignatureDoesNotMatch':
+        print(f"Erro de assinatura: {e}")
+    else:
+        print(f"Erro ao fazer upload: {e}")
+except Exception as e:
+    print(f"Erro inesperado: {e}")
