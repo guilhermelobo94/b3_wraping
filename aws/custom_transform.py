6e8050e70ce9dd614601df9bb6bda17da800e539
Matheus Moura Pinho <matheusmpinho@outlook.com>
2024-07-21 02:16:51 -0300

    Add files via upload

--- aws/custom_transform.py
+++ aws/custom_transform.py
@@ -1,5 +1,5 @@
 def MyTransform(glueContext, dfc) -> DynamicFrameCollection:
-    from pyspark.sql.functions import datediff, to_date, lit, col, regexp_replace
+    from pyspark.sql.functions import datediff, to_date, lit, col, regexp_replace, when
     from awsglue.dynamicframe import DynamicFrame, DynamicFrameCollection
     import pytz
     from datetime import datetime
@@ -9,21 +9,28 @@ def MyTransform(glueContext, dfc) -> DynamicFrameCollection:
 
     df = dfc.select(list(dfc.keys())[0]).toDF()
 
-    df_cleaned = df.withColumn("qtde_teorica",
-                               regexp_replace(col("qtde_teorica"), "\.", "")
-                               ).withColumn("qtde_teorica",
-                                            col("qtde_teorica").cast("int")
-                                            ).withColumn("participacao",
-                                                         regexp_replace(col("participacao"), ",", ".").cast("double")
-                                                         ).withColumn("participacao_acumulativo",
-                                                                      regexp_replace(col("participacao_acumulativo"),
-                                                                                     ",",
-                                                                                     ".").cast("double")
-                                                                      )
+    df_cleaned = df.withColumn(
+        "qtde_teorica_cleaned",
+        regexp_replace(col("qtde_teorica"), "[^\d]", "").cast("long")
+    ).withColumn(
+        "participacao_cleaned",
+        regexp_replace(col("participacao"), ",", ".").cast("double")
+    ).withColumn(
+        "participacao_acumulativo_cleaned",
+        regexp_replace(col("participacao_acumulativo"), ",", ".").cast("double")
+    )
 
-    df_cleaned = df_cleaned.withColumn('data_dados', to_date(col('data'), 'yyyy-MM-dd')).drop('data')
-    df_cleaned = df_cleaned.withColumn('data_atual', to_date(lit(now_sp), 'yyyy-MM-dd'))
-    df_cleaned = df_cleaned.withColumn('diferenca_dias', datediff(df_cleaned['data_atual'], df_cleaned['data_dados']))
+    df_cleaned = df_cleaned.withColumn(
+        'data_dados', to_date(col('data'), 'yyyy-MM-dd')
+    ).drop('data').withColumn(
+        'data_atual', to_date(lit(now_sp), 'yyyy-MM-dd')
+    ).withColumn(
+        'diferenca_dias', datediff(col('data_atual'), col('data_dados'))
+    )
+
+    df_cleaned = df_cleaned.withColumnRenamed('qtde_teorica_cleaned', 'qtde_teorica') \
+        .withColumnRenamed('participacao_cleaned', 'participacao') \
+        .withColumnRenamed('participacao_acumulativo_cleaned', 'participacao_acumulativo')
 
     result_dyf = DynamicFrame.fromDF(df_cleaned, glueContext, "result_dyf")
 
