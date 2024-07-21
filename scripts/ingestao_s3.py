5ac8fa1537a964e79083b454c2c513bf97fed159
Matheus Moura Pinho <matheusmpinho@outlook.com>
2024-07-21 01:33:57 -0300

    Update ingestao_s3.py

--- scripts/ingestao_s3.py
+++ scripts/ingestao_s3.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-aws_session_token=os.getenv('AWS_SESSION_TOKEN')
-aws_region = os.getenv('AWS_REGION')
+aws_access_key_id = os.getenv('aws_access_key_id')
+aws_secret_access_key = os.getenv('aws_secret_access_key')
+aws_session_token = os.getenv('aws_session_token')
+aws_region = os.getenv('aws_region')
 
 s3_client = boto3.client(
     's3',
